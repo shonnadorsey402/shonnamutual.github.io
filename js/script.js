bf8dfc4ae41a228d00707fc102d6688e9725e457
shonnamutual <42480578+shonnamutual@users.noreply.github.com>
2018-09-14 13:07:42 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
   $("#hideimage").fadeIn(300);
   $("#hidetext").fadeIn(750);
    $("body").animate({backgroundColor: "rgb( 20, 20, 20 )" }, 1000);
-  $("body").animate({backgroundColor: "rgb( 255, 255, 255 )" }, 2000);
+   $("body").animate({backgroundColor: "rgb( 255, 255, 255 )" }, 2000);
+   $("#hideimage", "#hidetext").toggle("puff");
 });
